7fde7f129ef3741c5c74c5e75fced34f2c1f45c9
viettv1 <viettv1@smartosc.com>
2023-08-21 09:50:51 +0700

    test vercel json

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,14 +1,18 @@
+import { NextResponse } from "next/server";
+
 export const runtime = "edge";
 
 export async function GET() {
   const date = new Date();
-
-  return new Response(date.toString(), {
-    status: 200,
-    headers: {
-      "Cache-Control": "public, s-maxage=30",
-      "CDN-Cache-Control": "public, s-maxage=30",
-      "Vercel-CDN-Cache-Control": "public, s-maxage=30",
-    },
-  });
+  return NextResponse.json(
+    { okeee: date },
+    {
+      status: 200,
+      headers: {
+        "Cache-Control": "public, s-maxage=30",
+        "CDN-Cache-Control": "public, s-maxage=30",
+        "Vercel-CDN-Cache-Control": "public, s-maxage=30",
+      },
+    }
+  );
 }
