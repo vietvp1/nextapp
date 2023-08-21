7f4f5918f3d32fdee5a64ac180f00b4fced514a7
viettv1 <viettv1@smartosc.com>
2023-08-21 10:03:54 +0700

    test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
 
-export const runtime = "edge";
-
 export async function GET() {
   const date = new Date();
   return NextResponse.json({ okeee: date });
